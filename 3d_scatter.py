bba3babd8c700c422eb5110f153ffc3e4e8d8258
Ben Lang <ben.lang@nyu.edu>
2022-02-04 12:31:21 -0800

    scatter plotting

--- 3d_scatter.py
+++ 3d_scatter.py
@@ -15,10 +15,45 @@ os.chdir(dir)
 ratings_fname = os.path.join(dir,'matched_queer.csv')
 ratings = pd.read_csv(ratings_fname)
 
+# establish participant list
+participants = pd.Series(range(len(ratings.groupby('Participant').count()))) + 1
+print(participants)
+
+
+## rating for visualization
 ratings['Condition'] = ratings['Condition'].astype('category')
 ratings['WAV'] = ratings['WAV'].astype('category')
-ratings.groupby("WAV")["Rating"].count()
+ratings_avg = ratings.groupby(['WAV', 'Condition'], as_index=False)['Rating'].mean()
+print(ratings_avg)
+ratings_avg_pivot = ratings_avg.pivot(index = 'WAV', columns = 'Condition', values = 'Rating').reset_index().rename_axis(None, axis=1)
+ratings_avg_pivot.set_index(['Condition']).VALUE.unstack().reset_index()
 
+# plot individual ratings
+fig = plt.figure(figsize = (10,7))
+ax = plt.axes(projection='3d')
+for participant in participants:
+    ratings_indiv = ratings[ratings['Participant'] == participant]
+    ratings_indiv_pivot = ratings_indiv.pivot(index = 'WAV', columns = 'Condition', values = 'Rating')
+    ax.scatter(ratings_indiv_pivot['gender_id'], ratings_pivot['sexual_orientation'], ratings_pivot['voice_id'], alpha=0.8, s=30)
+plt.title('Gender Identity, Sexual Orientation, and Vocie Identity Ratings (Indiv)', fontweight='bold')
+ax.set_xlabel('Gender ID', fontweight='bold')
+ax.set_ylabel('PSO', fontweight='bold')
+ax.set_zlabel('Voice (Masc-N-Femme)', fontweight='bold')
+plt.show()
+
+
+## plot averages
+fig = plt.figure(figsize = (10,7))
+ax = plt.axes(projection='3d')
+ax.scatter(ratings_pivot['gender_id'], ratings_pivot['sexual_orientation'], ratings_pivot['voice_id'], alpha=0.8, s=30, c='r')
+for i, txt in enumerate(ratings_pivot['gender_id']):
+    plt.annotate(txt, (ratings_pivot['gender_id'][i], ratings_pivot['sexual_orientation'][i], ratings_pivot['voice_id'][i]))
+plt.title('Gender Identity, Sexual Orientation, and Vocie Identity Ratings (Avg)', fontweight='bold')
+ax.set_xlabel('Gender ID', fontweight='bold')
+ax.set_ylabel('PSO', fontweight='bold')
+ax.set_zlabel('Voice (Masc-N-Femme)', fontweight='bold')
+# ax.legend(handles=ratings_pivot['WAV'])
+plt.show()
 
 # ## check if ratings already made and load
 # if os.path.isfile(ratings_fname):
