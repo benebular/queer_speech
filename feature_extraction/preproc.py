63c3899ed40d7c846a9bc6ec85aea40206776e53
Ben <ben.lang@nyu.edu>
2022-03-19 12:13:09 -0700

    added in spectral moment for 6 fricatives to ratings_all

--- feature_extraction/preproc.py
+++ feature_extraction/preproc.py
@@ -19,10 +19,22 @@ os.chdir(dir)
 vs_fname = os.path.join(dir,'feature_extraction','vs_ms_output.csv')
 matches_fname = os.path.join(dir, 'qualtrics_data/mark1_jan28/','matches_queer_speech.csv')
 queer_ratings_fname = os.path.join(dir, 'qualtrics_data/mark1_jan28/','matched_queer.csv')
+spectral_S_fname = os.path.join(dir,'feature_extraction','logfile_S.csv')
+spectral_SH_fname = os.path.join(dir,'feature_extraction','logfile_SH.csv')
+spectral_Z_fname = os.path.join(dir,'feature_extraction','logfile_Z.csv')
+spectral_JH_fname = os.path.join(dir,'feature_extraction','logfile_JH.csv')
+spectral_F_fname = os.path.join(dir,'feature_extraction','logfile_F.csv')
+spectral_V_fname = os.path.join(dir,'feature_extraction','logfile_V.csv')
 
 vs = pd.read_csv(vs_fname)
 matches = pd.read_csv(matches_fname)
 queer_ratings = pd.read_csv(queer_ratings_fname)
+spectral_S = pd.read_csv(spectral_S_fname, header=None)
+spectral_SH = pd.read_csv(spectral_SH_fname, header=None)
+spectral_Z = pd.read_csv(spectral_Z_fname, header=None)
+spectral_JH = pd.read_csv(spectral_JH_fname, header=None)
+spectral_F = pd.read_csv(spectral_F_fname, header=None)
+spectral_V = pd.read_csv(spectral_V_fname, header=None)
 vs.rename(columns = {'Filename':'WAV'}, inplace = True)
 # view all labels in each file
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
@@ -34,6 +46,38 @@ for i in vs['WAV']:
     x = i[:-4]
     new_names.append(x)
 vs['WAV'] = new_names
+
+## rename cols in spectral moment dfs
+spectral_dfs = [spectral_S,spectral_SH,spectral_Z,spectral_JH,spectral_F,spectral_V]
+for i in spectral_dfs:
+    i.columns = ['WAV','start','duration','intensity','cog','sdev','skew','kurt','delete_me'] # add in colnames
+    i['WAV'] = i['WAV'].str.replace(r'\.wav', '') # get rid of .wav suffix
+    i['WAV'] = i['WAV'].str.replace(r'\ ', '') # get rid of .wav suffix
+    del i['delete_me'] # delete random NaN column from raw data
+    # the praat script puts the colmn name in as the nan value instead of a nan or a 0 so the following line changes all of those to nans for analysis
+    i[['start','duration','intensity','cog','sdev','skew','kurt']] = i[['start','duration','intensity','cog','sdev','skew','kurt']].apply(pd.to_numeric,errors='coerce') # change all copied strings in data to NaNs
+
+# change col names to unique spectral segments
+spectral_S = spectral_S.add_prefix('spectral_S_')
+spectral_S = spectral_S.rename(columns={'spectral_S_WAV':'WAV'})
+spectral_Z = spectral_Z.add_prefix('spectral_Z_')
+spectral_Z = spectral_Z.rename(columns={'spectral_Z_WAV':'WAV'})
+spectral_F = spectral_F.add_prefix('spectral_F_')
+spectral_F = spectral_F.rename(columns={'spectral_F_WAV':'WAV'})
+spectral_V = spectral_V.add_prefix('spectral_V_')
+spectral_V = spectral_V.rename(columns={'spectral_V_WAV':'WAV'})
+spectral_SH = spectral_SH.add_prefix('spectral_SH_')
+spectral_SH = spectral_SH.rename(columns={'spectral_SH_WAV':'WAV'})
+spectral_JH = spectral_JH.add_prefix('spectral_JH_')
+spectral_JH = spectral_JH.rename(columns={'spectral_JH_WAV':'WAV'})
+
+# average over spectral segments, leaves nans in place for files without a spectral item
+spectral_S = spectral_S.groupby('WAV', as_index=False).mean()
+spectral_Z = spectral_Z.groupby('WAV', as_index=False).mean()
+spectral_F = spectral_F.groupby('WAV', as_index=False).mean()
+spectral_V = spectral_V.groupby('WAV', as_index=False).mean()
+spectral_SH = spectral_SH.groupby('WAV', as_index=False).mean()
+spectral_JH = spectral_JH.groupby('WAV', as_index=False).mean()
 
 # segment durations
 vs['duration'] = vs['seg_End'] - vs['seg_Start']
@@ -59,13 +103,20 @@ F0_10 = pd.concat([pd.DataFrame({'WAV':duration_pivot.index}), F0_10], axis = 1)
 F0_subtraction = F0_90['F0_90'] - F0_10['F0_10']
 F0_range = pd.DataFrame({'WAV':F0_avg['WAV'],'F0_range':F0_subtraction})
 
-# concatenate F0 and duration dfs into ratings df
+# concatenate all into ratings df
 ratings_all = pd.merge(queer_ratings, F0_avg, on='WAV')
 ratings_all = pd.merge(ratings_all, F0_std, on='WAV')
 ratings_all = pd.merge(ratings_all, F0_range, on='WAV')
 ratings_all = pd.merge(ratings_all, F0_90, on='WAV')
 ratings_all = pd.merge(ratings_all, F0_10, on='WAV')
 ratings_all = pd.merge(ratings_all, duration, on='WAV')
+ratings_all = pd.merge(ratings_all, spectral_S, on='WAV')
+ratings_all = pd.merge(ratings_all, spectral_Z, on='WAV')
+ratings_all = pd.merge(ratings_all, spectral_F, on='WAV')
+ratings_all = pd.merge(ratings_all, spectral_V, on='WAV')
+ratings_all = pd.merge(ratings_all, spectral_SH, on='WAV')
+ratings_all = pd.merge(ratings_all, spectral_JH, on='WAV')
+
 
 # formants for vowels
 formant_labels = ['sF1','sF2','sF3','sF4','sB1','sB2','sB3','sB4']
@@ -79,12 +130,12 @@ for val in formant_labels:
 vowel_labels = ['AA','AE','AH','AO','AW','AX','AY','EH','EY','IH','IY','OW','OY','UH','UW']
 # vowels_B4 = vowels_pivot[['AA','AE','AH','AO','AW','AX','AY','EH','EY','IH','IY','OW','OY','UH','UW']]
 
-
-
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
 #     print(vowels_pivot[['AA','AE','AH','AO','AW','AX','AY','EH','EY','IH','IY','OW','OY','UH','UW']])
 
 ### EXPORT ###
+sc_WAV_count = ratings_all['WAV'].nunique()
+print("Sanity check: There are %s unique WAV files in ratings_all."%sc_WAV_count)
 print ("Saving ratings_all as ratings_features_all.csv")
 ratings_all.to_csv(os.path.join(dir,'feature_extraction','ratings_features_all.csv'), index=True, encoding='utf-8')
 
@@ -125,6 +176,21 @@ plt.savefig(os.path.join(dir,'figs', 'F0_raincloud.png'), bbox_inches='tight', d
 # plt.title("Ratings of Gender Identity by Proximity")
 # # plt.show()
 # plt.savefig(os.path.join(dir,'figs', 'gender_id_prox_raincloud.png'), bbox_inches='tight', dpi=300)
+
+# Ratings by Condition
+plot_gender_id_rating = pd.DataFrame({'group':'Gender Identity', 'Rating': gender_id['Rating']}).drop_duplicates()
+plot_sexual_orientation_rating = pd.DataFrame({'group':'PSO', 'Rating': sexual_orientation['Rating']}).drop_duplicates()
+plot_voice_id_rating = pd.DataFrame({'group':'Voice Typicality', 'Rating': voice_id['Rating']}).drop_duplicates()
+plot_conditions = pd.concat([plot_sexual_orientation_rating, plot_gender_id_rating, plot_voice_id_rating])
+
+dx="group"; dy="Rating"; ort="h"; pal = sns.color_palette(n_colors=3); sigma = .2
+f, ax = plt.subplots(figsize=(7, 5))
+pt.RainCloud(x = dx, y = dy, data = plot_conditions, palette = pal, bw = sigma,
+                 width_viol = .6, ax = ax, orient = ort)
+
+plt.title("Avg Ratings Distribution by Condition")
+# plt.show()
+plt.savefig(os.path.join(dir,'figs', 'ratingsbycondition_raincloud.png'), bbox_inches='tight', dpi=300)
 
 
 ### F0 AVG ####create scatterplot with regression line and confidence interval lines
@@ -182,6 +248,64 @@ axes.set_ylabel('Avg F0')
 plt.legend(labels=['Gender ID','Sexual Orientation','Voice ID'])
 plt.savefig(os.path.join(dir,'figs', 'F0_avgbycondition_overlay.png'), bbox_inches='tight', dpi=300)
 # plt.show()
+
+
+#### COG ####
+gender_id_avg_rating = gender_id.groupby('WAV', as_index=False)['Rating'].mean()
+gender_id_avg_F0 = gender_id.groupby('WAV', as_index=False)['F0_mean'].mean()
+gender_id_F0 = pd.merge(gender_id_avg_rating, gender_id_avg_F0, on='WAV')
+
+sexual_orientation_avg_rating = sexual_orientation.groupby('WAV', as_index=False)['Rating'].mean()
+sexual_orientation_avg_F0 = sexual_orientation.groupby('WAV', as_index=False)['F0_mean'].mean()
+sexual_orientation_F0 = pd.merge(sexual_orientation_avg_rating, sexual_orientation_avg_F0, on='WAV')
+
+voice_id_rating = voice_id.groupby('WAV', as_index=False)['Rating'].mean()
+voice_id_avg_F0 = voice_id.groupby('WAV', as_index=False)['F0_mean'].mean()
+voice_id_F0 = pd.merge(voice_id_rating, voice_id_avg_F0, on='WAV')
+
+# plot
+fig, axes = plt.subplots(1, 3)
+fig.subplots_adjust(hspace=0.5)
+fig.set_size_inches(16, 6)
+fig.suptitle("Avg F0 by Rating", fontsize=20, fontweight='bold')
+fig.subplots_adjust( top = 0.85 )
+
+axes[0].set_title('Gender Identity')
+axes[0].set_xlim(1,7)
+sns.regplot(data=gender_id_F0, x='Rating', y='F0_mean', ax=axes[0], color='#d55e00')
+axes[0].set_xlabel('Rating (1-Male, 7-Female)')
+axes[0].set_ylabel('Avg F0')
+
+axes[1].set_title('Sexual Orientation')
+axes[1].set_xlim(1,7)
+sns.regplot(data=sexual_orientation_F0, x='Rating', y='F0_mean', ax=axes[1], color='#0072b2')
+axes[1].set_xlabel('Rating (1-Homo, 7-Het)')
+axes[1].set_ylabel('')
+
+axes[2].set_title('Voice Identity')
+axes[2].set_xlim(1,7)
+sns.regplot(data=voice_id_F0, x='Rating', y='F0_mean', ax=axes[2], color='#009e73')
+axes[2].set_xlabel('Rating (1-Masc, 7-Femme)')
+axes[2].set_ylabel('')
+
+# plt.show()
+plt.savefig(os.path.join(dir,'figs', 'F0_avgbycondition.png'), bbox_inches='tight', dpi=300)
+plt.clf()
+
+# overlay
+fig, axes = plt.subplots()
+fig.set_size_inches(18, 10)
+axes.set_title('Avg F0 by Condition Rating', fontsize=20, fontweight='bold')
+axes.set_xlim(1,7)
+sns.regplot(data=gender_id_F0, x='Rating', y='F0_mean', color='#d55e00')
+sns.regplot(data=sexual_orientation_F0, x='Rating', y='F0_mean', color='#0072b2')
+sns.regplot(data=voice_id_F0, x='Rating', y='F0_mean', color='#009e73')
+axes.set_xlabel('Rating (1-7)')
+axes.set_ylabel('Avg F0')
+plt.legend(labels=['Gender ID','Sexual Orientation','Voice ID'])
+plt.savefig(os.path.join(dir,'figs', 'F0_avgbycondition_overlay.png'), bbox_inches='tight', dpi=300)
+# plt.show()
+
 
 
 #### proximity
